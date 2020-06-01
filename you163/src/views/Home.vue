aa5f39aecee71dc65d1eaf1ed56bb2036ae7a534
zl21 <44433851+zl21@users.noreply.github.com>
2020-06-01 15:17:24 +0800

    vue cli 4
    Mention vue create you163.

---
+++ you163/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- 从当前的组件Home传一个值到HelloWorld组件去，引入、注册、使用并属性传参 -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
