78269dd6e0b90a0ddb18f07b9d80a7231b80e425
zl21 <44433851+zl21@users.noreply.github.com>
2020-06-02 09:09:16 +0800

    IndexWrap
    IndexWrap.vue header layout end,include logo,search and loginButton.

---
+++ you163/src/components/IndexWrap.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="header">
+    <van-row type="flex" justify="space-between">
+      <van-col span="5" class="header_h">
+        <img class="logo" src="../assets/logo.png" />
+      </van-col>
+      <van-col span="15" class="search">
+        <van-icon name="search" size=".5rem" />
+        <span>搜索商品, 共29863款好物</span>
+      </van-col>
+      <van-col span="3" class="header_h">
+        <van-button class="loginBtn" type="primary" size="mini">登录</van-button>
+      </van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexWrap",
+  // props: {
+  //   msg: String
+  // }
+  data() {
+    return {
+      msg: "zxcv"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  padding: 0.21333rem 0.4rem;
+}
+.logo {
+  width: 1.84rem;
+  height: 0.53333rem;
+}
+.search {
+  height: 0.74667rem;
+  font-size: 0.37333rem;
+  background-color: #ededed;
+  border-radius: 0.10667rem;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding-top: .081rem;
+}
+.loginBtn {
+  width: 0.98667rem;
+  height: 0.53333rem;
+  line-height: 0.53333rem;
+  text-align: center;
+  color: #dd1a21;
+  border: 1px solid #dd1a21;
+  border-radius: 0.10667rem;
+  margin-left: 0.21333rem;
+  font-size: 0.32rem;
+  background: #fff;
+}
+.header_h {
+  font-size: 0.5rem;
+}
+</style>
--- you163/src/main.js
+++ you163/src/main.js
@@ -1,8 +1,14 @@
+import './assets/reset.css'
 import './assets/rem'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import { Button, Col, Row, Icon } from 'vant'
+
+Vue.use(Button).use(Col).use(Row).use(Icon);
 
 Vue.config.productionTip = false
 
