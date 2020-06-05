bf0ed7c872120094669d7c511d2bdb13d976e04d
zl21 <44433851+zl21@users.noreply.github.com>
2020-06-05 18:39:39 +0800

    footerTabbar

--- you163/src/components/IndexWrap.vue
+++ you163/src/components/IndexWrap.vue
@@ -197,6 +197,41 @@
         />
       </van-grid>
     </div>
+    <!-- footerTabbar -->
+    <div class="footerTabbar">
+      <van-tabbar active-color="#f00" v-model="active">
+        <van-tabbar-item>
+          <span>首页</span>
+          <template #icon="props">
+            <div :class="props.active ? icon.i_home_a : icon.i_home"></div>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>分类</span>
+          <template #icon="props">
+            <div :class="props.active ? icon.i_sort_a : icon.i_sort"></div>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>值得买</span>
+          <template #icon="props">
+            <div :class="props.active ? icon.i_worthy_a : icon.i_worthy"></div>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>购物车</span>
+          <template #icon="props">
+            <div :class="props.active ? icon.i_cart_a : icon.i_cart"></div>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item>
+          <span>个人</span>
+          <template #icon="props">
+            <div :class="props.active ? icon.i_personal_a : icon.i_personal"></div>
+          </template>
+        </van-tabbar-item>
+      </van-tabbar>
+    </div>
   </div>
 </template>
 
@@ -216,6 +251,18 @@ export default {
       tabArr: [],
       swiperImgArr: [],
       time_val: 30 * 60 * 60 * 1000,
+      icon: {
+        i_home: "footerIcon footer_i_home",
+        i_home_a: "footerIcon footer_i_home_a",
+        i_sort: "footerIcon footer_i_sort",
+        i_sort_a: "footerIcon footer_i_sort_a",
+        i_worthy: "footerIcon footer_i_worthy",
+        i_worthy_a: "footerIcon footer_i_worthy_a",
+        i_cart: "footerIcon footer_i_cart",
+        i_cart_a: "footerIcon footer_i_cart_a",
+        i_personal: "footerIcon footer_i_personal",
+        i_personal_a: "footerIcon footer_i_personal_a",
+      },
       categoryGridArr: [
         {
           img_url:
@@ -607,5 +654,45 @@ export default {
   text-align: center;
   background-color: #333;
   border-radius: 0.05333rem;
+}
+
+// footer
+.footerIcon {
+  width: 0.53rem;
+  height: 0.53rem;
+  margin: 0 auto;
+  background-image: url("../assets/footer_icon.png");
+  background-size: 0.53333rem;
+  background-repeat: no-repeat;
+}
+.footer_i_home{
+  background-position: 0 -251px;
+}
+.footer_i_home_a{
+  background-position: 0 -200px;
+}
+.footer_i_sort{
+  background-position: 0 -151px;
+}
+.footer_i_sort_a{
+background-position: 0 -100px;
+}
+.footer_i_worthy{
+background-position: 0 -351px;
+}
+.footer_i_worthy_a{
+background-position: 0 -300px;
+}
+.footer_i_cart{
+background-position: 0 -51px;
+}
+.footer_i_cart_a{
+background-position: 0 -0px;
+}
+.footer_i_personal{
+background-position: 0 -251px;
+}
+.footer_i_personal_a{
+background-position: 0 -401px;
 }
 </style>
