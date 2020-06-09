407270d7f147462c0bfefcff85fc353e213e80fa
zl21 <44433851+zl21@users.noreply.github.com>
2020-06-09 15:15:09 +0800

    goodsSidebar
    模拟商品数据，实现返回商品数据的接口(eq:category_list_x)，点击侧边栏导航切换商品列表……

--- you163/middleware/middleware.js
+++ you163/middleware/middleware.js
@@ -72,6 +72,73 @@ app.get('/get_swiperImg', (req, res) => {
 	res.send(_n)
 })
 
+// 分类页商品列表点击请求
+app.get('/category_list_0', (req, res) => {
+	let _d = [{
+		goods_img: 'https://yanxuan-item.nosdn.127.net/0afcd9dd065aa207e56d34dd85aa136e.png',
+		goods_name: '睡出丝滑娇肤，100%桑蚕丝防螨提花被 薄被',
+		goods_price: 679
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/c552b8bf17d88aa9bba6972f1d449483.png',
+		goods_name: '自带清凉，黑科技持久冷感垫 三件套',
+		goods_price: 159
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/55793ff7a0b3eb47b4b562cabd1670d9.png',
+		goods_name: '可折叠爽滑冰丝席 三件套',
+		goods_price: 189
+	}];
+
+	res.send(_d);
+});
+
+app.get('/category_list_1', (req, res) => {
+	let _d = [{
+		goods_img: 'https://yanxuan-item.nosdn.127.net/aa8d1a3f633f954474e421ef627c075a.png',
+		goods_name: '法国原产条纹圆领短袖T恤情侣款',
+		goods_price: 578
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/8563f498cec5b5f9474c8daadce4ca9f.png',
+		goods_name: '男式舒适丝棉条纹短袖衬衫',
+		goods_price: 209
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/16e78d60046c37375f8161e947258a70.png',
+		goods_name: '女式方扣中跟乐福鞋',
+		goods_price: 349
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/e3b6d3274397cdc5c1632ebf63059897.png',
+		goods_name: '男式无缝休闲运动T恤',
+		goods_price: 49.9
+	}];
+
+	res.send(_d);
+});
+
+app.get('/category_list_2', (req, res) => {
+	let _d = [{
+		goods_img: 'https://yanxuan-item.nosdn.127.net/ab5de9e34349e85581487446ea44b3e9.png',
+		goods_name: '夏季宵食吮指好味，盱眙小龙虾4-6钱 3斤装',
+		goods_price: 109
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/926865f19bd4b3dc10285cb8307144e6.png',
+		goods_name: '逛吃逛吃的青春季，想见你 情侣零食礼袋',
+		goods_price: 99
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/9e3ec5ff3fc254c30583ad9b65d39a9a.png',
+		goods_name: '山药脆片 烧烤味 70克*3袋',
+		goods_price: 19
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/23a9fba86c3942f1a574731792854bc2.png',
+		goods_name: '女式无缝运动T恤',
+		goods_price: 49.9
+	}, {
+		goods_img: 'https://yanxuan-item.nosdn.127.net/927f79fa2979260fa8e87fdb2187f701.png',
+		goods_name: '女式低强度无缝运动文胸',
+		goods_price: 49.9
+	}];
+
+	res.send(_d);
+});
+
 app.listen(3344, () => {
 	console.log("3344接口启用中！");
 })
--- you163/src/components/CateList.vue
+++ you163/src/components/CateList.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="category">
+    <!-- 左侧导航区 -->
     <div class="sidebarWrap">
-      <van-sidebar v-model="activeKey"><van-sidebar-item v-for="(item,index) in sidebarArr" :key="index" :title="item.txt" /></van-sidebar>
+      <van-sidebar v-model="activeKey" @change="onChangeSidebar">
+        <van-sidebar-item v-for="(item,index) in sidebarArr" :key="index" :title="item.txt" />
+      </van-sidebar>
     </div>
-
+    <!-- 右侧：对应某导航类别的商品展示区 -->
+    <div class="goodsSidebar">
+      <div class="goodsItem" v-for="(item,index) in categoryListObj" :key="index">
+        <img :src="item.goods_img" />
+        <p>{{item.goods_name}}</p>
+        <p>{{item.goods_price}}</p>
+      </div>
+    </div>
     <!-- footerTabbar -->
     <FooterBar />
   </div>
@@ -11,6 +21,7 @@
 
 <script>
 import FooterBar from "./FooterBar";
+import axios from "axios";
 export default {
   name: "CateList",
   data() {
@@ -51,14 +62,51 @@ export default {
         {
           txt: "严选全球"
         }
-      ]
+      ],
+      categoryListObj: null
     };
   },
   components: {
     FooterBar
+  },
+  created() {
+    //   默认展示第0个
+    axios.get("http://localhost:3344/category_list_0").then(_d => {
+      this.categoryListObj = _d.data;
+    });
+  },
+  methods: {
+    onChangeSidebar(_inx) {
+      //   console.log(_inx);
+      axios.get("http://localhost:3344/category_list_" + _inx).then(_d => {
+        console.log(_d.data);
+        this.categoryListObj = _d.data;
+      });
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+.category {
+  position: relative;
+  padding-left: 2.15rem;
+}
+.sidebarWrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.goodsItem {
+  float: left;
+  width: 44%;
+  margin: 0 0.2rem 0.44rem 0.2rem;
+  font-size: 0.37333rem;
+  overflow: hidden;
+}
+.goodsItem img {
+  width: 100%;
+  height: 4.6rem;
+  border-radius: 0.05333rem 0.05333rem 0 0;
+}
 </style>>
